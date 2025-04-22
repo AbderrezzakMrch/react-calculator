e2dc0f242f0258685a098a7b3035216f0bdcccf5
Abdou.exe <147605515+AbderrezzakMrch@users.noreply.github.com>
2025-04-22 20:34:24 +0100

    Final App

--- src/components/Calculater/Calc.jsx
+++ src/components/Calculater/Calc.jsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import './Calc.css'
 
 
 function Calculater() {
 
-
+const [input, setInput] = useState("")
+const [result, setResult] = useState(0)
+    const handleCalc = (value) => {
+        if (value === '='){
+            try{
+                setResult(eval(input))
+                setInput("")
+            }catch{
+                setResult("ERROR")
+            }
+        }else if (value === 'C') {
+            setInput("")
+            setResult(0)
+        }else {
+            setInput(input + value)
+        }
+    };
     return(
-
         <div className="container">
             <h1 className="title">Calculater in <span>React</span></h1>
-            <div className="calc">
-                <div className="row1">
-                    <div className="box">7</div>
-                    <div className="box">4</div>
-                    <div className="box">1</div>
-                    <div className="box">+/-</div>
+            <div className="calculator">
+                <div className="display">
+                    <div className="input">{input}</div>
+                    <div className="result">{result}</div>
                 </div>
-                <div className="row2">
-                    <div className="box">8</div>
-                    <div className="box">5</div>
-                    <div className="box">2</div>
-                    <div className="box">0</div>
-                </div>
-                <div className="row3">
-                    <div className="box">9</div>
-                    <div className="box">6</div>
-                    <div className="box">3</div>
-                    <div className="box">.</div>
-                </div>
-                <div className="row4">
-                    <div className="box">/</div>
-                    <div className="box">*</div>
-                    <div className="box">-</div>
-                    <div className="box">+</div>
+                <div className="buttons">
+                    <button onClick={()=> handleCalc("7")}>7</button>
+                    <button onClick={()=> handleCalc("8")}>8</button>
+                    <button onClick={()=> handleCalc("9")}>9</button>
+                    <button onClick={()=> handleCalc("/")}>/</button>
+
+                    <button onClick={()=> handleCalc("4")}>4</button>
+                    <button onClick={()=> handleCalc("5")}>5</button>
+                    <button onClick={()=> handleCalc("6")}>6</button>
+                    <button onClick={()=> handleCalc("*")}>*</button>
+
+                    <button onClick={()=> handleCalc("1")}>1</button>
+                    <button onClick={()=> handleCalc("2")}>2</button>
+                    <button onClick={()=> handleCalc("3")}>3</button>
+                    <button onClick={()=> handleCalc("-")}>-</button>
+
+                    <button onClick={()=> handleCalc("0")}>0</button>
+                    <button onClick={()=> handleCalc(".")}>.</button>
+                    <button onClick={()=> handleCalc("=")}>=</button>
+                    <button onClick={()=> handleCalc("+")}>+</button>
+
+                    <button className="clear" onClick={()=> handleCalc("C")}>C</button>
                 </div>
             </div>
         </div>
